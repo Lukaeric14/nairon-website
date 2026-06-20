8ddb418308b50ac8c8c5d6855ae0bb6799bc1b9c
Obaid <ourrahmaan@gmail.com>
2026-06-20 14:59:35 +0400

    Update Hive desktop download versions to 0.6.0

--- apps/web/src/routes/download.tsx
+++ apps/web/src/routes/download.tsx
@@ -14,10 +14,10 @@ const appStoreConnect =
 	"https://appstoreconnect.apple.com/apps/6762267127/testflight/ios";
 
 const fallbackDesktop = {
-	macVersion: "0.5.108",
-	windowsVersion: "0.5.108",
-	macDmg: `${desktopFeedBase}/Hive-0.5.108-universal.dmg`,
-	windowsExe: `${desktopFeedBase}/Hive-Setup-0.5.108.exe`,
+	macVersion: "0.6.0",
+	windowsVersion: "0.6.0",
+	macDmg: `${desktopFeedBase}/Hive-0.6.0-universal.dmg`,
+	windowsExe: `${desktopFeedBase}/Hive-Setup-0.6.0.exe`,
 	macManifest: `${desktopFeedBase}/latest-mac.yml`,
 	windowsManifest: `${desktopFeedBase}/latest.yml`,
 };
